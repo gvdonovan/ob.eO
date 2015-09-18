0a4569037b0d97e69fbc902790b82164c01523ac
jadams <jadams@optimalblue.com>
2015-09-18 14:13:28 -0500

    new api route

--- Sites/eO.Web/wwwroot/app/search/services/quickSearchService.js
+++ Sites/eO.Web/wwwroot/app/search/services/quickSearchService.js
@@ -148,39 +148,6 @@
                         }
                     ]
                 }
-                //,
-                //{
-                //    title: 'Conforming 3/1 ARM',
-                //    items: [
-                //        {
-                //            rate: 3.875,
-                //            discPoints: 1875.00,
-                //            apr: 3.996,
-                //            months: 360,
-                //            payment: 1411.00,
-                //            closingCost: 4675.00,
-                //            rebate: 0.00
-                //        },
-                //        {
-                //            rate: 4,
-                //            discPoints: 1700.00,
-                //            apr: 3.999,
-                //            months: 360,
-                //            payment: 1511.00,
-                //            closingCost: 3075.00,
-                //            rebate: 10.00
-                //        },
-                //        {
-                //            rate: 4.2,
-                //            discPoints: 1500.00,
-                //            apr: 4.100,
-                //            months: 360,
-                //            payment: 1611.00,
-                //            closingCost: 500.00,
-                //            rebate: 220.00
-                //        }
-                //    ]
-                //}
             ];
 
             return results;
@@ -191,144 +158,34 @@
             userId = userId ? userId : 1
             formId = formId ? formId : 1
 
-            var url = '//localhost:62900/api/search/getFormData/' + entityId + '/' + userId + '/' + formId;
+            var url = '//localhost:63312/api/config/search/Inputs/' + entityId + '/' + formId;
             return $http.get(url)
-                 .then(function (response) {
-                     return response.data;
-                 }, function (response) {
-                     console.warn('error' + response);
-                 });
+                .then(function (response) {
 
-            //var data = null;
-            //data = {
-            //    fields: [
-            //        {
-            //            key: 'occupancy',
-            //            type: 'select',
-            //            templateOptions: {
-            //                label: 'Occupancy',
-            //                options: [
-            //                    {
-            //                        label: 'Owner Occupied',
-            //                        id: 'owner_occupied'
-            //                    },
-            //                    {
-            //                        label: 'Other',
-            //                        id: 'other'
-            //                    },
-            //                    {
-            //                        label: 'Biff',
-            //                        id: 'biff'
-            //                    },
-            //                ],
-            //                "valueProp": 'id',
-            //                "labelProp": 'label'
-            //            }
-            //        },
-            //        {
-            //            key: 'propertyType',
-            //            type: 'select',
-            //            templateOptions: {
-            //                label: 'Property Type',
-            //                options: [
-            //                    {
-            //                        label: 'Single Family',
-            //                        value: 'single_family'
-            //                    },
-            //                    {
-            //                        label: 'PUD',
-            //                        value: 'pud'
-            //                    },
-            //                    {
-            //                        label: 'Multi-Family',
-            //                        value: 'multi_family'
-            //                    },
-            //                    {
-            //                        label: 'Manufactured / Single Wide',
-            //                        value: 'manufactured_single_wide'
-            //                    },
-            //                    {
-            //                        label: 'Manufactured / Double Wide',
-            //                        value: 'manufactured_double_wide'
-            //                    },
-            //                    {
-            //                        label: 'Timeshare',
-            //                        value: 'timeshare'
-            //                    },
-            //                    {
-            //                        label: 'Condotel',
-            //                        value: 'condotel'
-            //                    },
-            //                    {
-            //                        label: 'Non-warrantable Condo',
-            //                        value: 'non_warrantable_condo'
-            //                    },
-            //                    {
-            //                        label: 'Modular',
-            //                        value: 'modular'
-            //                    },
-            //                ],
-            //                valueProp: 'value',
-            //                labelProp: 'label',
-            //                required: false
-            //            }
-            //        },
-            //        {
-            //            key: 'loanPurpose',
-            //            type: 'input',
-            //            templateOptions: {
-            //                label: 'Loan Purpose',
-            //                type: 'text',
-            //                required: false
-            //            }
-            //        },
-            //        {
-            //            key: 'purchasePrice',
-            //            type: 'input',
-            //            templateOptions: {
-            //                label: 'Purchase Price',
-            //                type: 'number',
-            //                placeholder: '0.00',
-            //                addonLeft: {
-            //                    text: '$'
-            //                },
-            //                required: false
-            //            }
-            //        },
-            //        {
-            //            key: 'downPayment',
-            //            type: 'input',
-            //            templateOptions: {
-            //                label: 'Down Payment',
-            //                type: 'number',
-            //                placeholder: '0.00',
-            //                addonLeft: {
-            //                    text: '$'
-            //                },
-            //                required: false
-            //            }
-            //        },
-            //        {
-            //            key: 'zip',
-            //            type: 'input',
-            //            templateOptions: {
-            //                label: 'Zip',
-            //                type: 'text',
-            //                required: false
-            //            }
-            //        },
-            //        {
-            //            key: 'creditScore',
-            //            type: 'input',
-            //            templateOptions: {
-            //                label: 'Credit Score',
-            //                type: 'text',
-            //                required: false
-            //            }
-            //        }
-            //    ]
-            //};
-            //return data;
+                    //if purchase
+                    var purchasePrice = _.findWhere(response.data.fields, { key: 'PurchasePrice' });
+                    purchasePrice['hideExpression'] = "model.LoanPurpose === '2' || model.LoanPurpose === '3'";
+                    var downPayment = _.findWhere(response.data.fields, { key: 'DownPayment' });
+                    downPayment['hideExpression'] = "model.LoanPurpose === '2' || model.LoanPurpose === '3'";
+
+                    //if not purchase
+                    var loanAmount = _.findWhere(response.data.fields, { key: 'LoanAmount' });
+                    loanAmount['expressionProperties'] = {
+                        'hide': function ($viewValue, $modelValue, scope) {
+                            return scope.model.LoanPurpose === '1' || !scope.model.LoanPurpose
+                        }
+                    };
+                    var estimatedValue = _.findWhere(response.data.fields, { key: 'EstimatedValue' });
+                    estimatedValue['expressionProperties'] = {
+                        'hide': function ($viewValue, $modelValue, scope) {
+                            return scope.model.LoanPurpose === '1' || !scope.model.LoanPurpose
+                        }
+                    };
+
+                    return response.data;
+                }, function (response) {
+                    console.warn('error' + response);
+                });
         }
     }
 })();
